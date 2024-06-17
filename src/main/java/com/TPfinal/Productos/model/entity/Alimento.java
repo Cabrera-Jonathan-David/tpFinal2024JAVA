e5363ada0f1211bc34163e4f03953a8e00811e9a
Cabrera-Jonathan-David <124104724+Cabrera-Jonathan-David@users.noreply.github.com>
2024-06-17 16:31:45 -0300

    Add files via upload

---
+++ src/main/java/com/TPfinal/Productos/model/entity/Alimento.java
@@ -0,0 +1,19 @@
+package com.TPfinal.Productos.model.entity;
+
+public class Alimento extends Producto{
+    private String vencimiento;
+
+    public Alimento(String nombre, Integer precio, String marca, Integer stockDisponible, String vencimiento) {
+        super(nombre, precio, marca, stockDisponible);
+        this.vencimiento = vencimiento;
+    }
+
+    public String getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(String vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+
+}
